74ec6a7d78ecf7ed6f06bb758207ddf2579ff7f7
liujun <ajun_8892@163.com>
2020-09-28 07:18:24 +0800

    init

---
+++ App.vue
@@ -0,0 +1,26 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/* 引入uniapp的css库 */
+	@import url("./common/uni.css");
+	/* 引入公共的css库 */
+	@import url("./common/common.css");
+	/* 引入free.css库 */
+	@import url("./common/free.css");
+	/* 引入icon-font字体图标库 */
+	@import url("./common/icon.css");
+	/* 引入动画库 */
+	@import url("./common/animate.css");
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,45 @@
+<template>
+	<view class="">
+		<view class="flex font-lg text-white">
+			<view class="flex flex-1 bg-success align-center justify-center">
+				1
+			</view>
+			<view class="flex flex-1 bg-danger align-center justify-center">
+				2
+			</view>
+		</view>
+		<view class="flex justify-center align-center animated" hover-class="rubberBand">
+			<view class="iconfont iconjinbi text-danger" style="font-size: 100rpx;">
+				
+			</view>
+		</view>
+		
+	</view>
+	
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		onNavigationBarSearchInputClicked(e) {
+			console.log(e);
+		},
+		onNavigationBarButtonTap(e) {
+			console.log(e);
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
